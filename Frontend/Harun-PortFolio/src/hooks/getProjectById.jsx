af612fa77a6bc263898be112ab443aaa20a8a71d
Harun-Frontend <hk2665550@gmail.com>
2026-04-16 19:15:37 +0500

    Update API endpoint of get single project by id

--- Frontend/Harun-PortFolio/src/hooks/getProjectById.jsx
+++ Frontend/Harun-PortFolio/src/hooks/getProjectById.jsx
@@ -11,7 +11,7 @@ const useGetProjectById = (id) => {
     const fetchProject = async () => {
       try {
         const res = await axios.get(
-          `http://localhost:8000/api/projects/getProject/${id}`
+          `https://harun-portfolio-backend.onrender.com/api/projects/getProject/${id}`
         );
 
         dispatch(setSingleProject(res.data.project));
